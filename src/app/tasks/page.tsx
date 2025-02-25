6f6e839fa6b60c390d7009b92afe975e628525cc
Rahul Gusai <rgusai97@gmail.com>
2025-02-25 09:47:59 +0530

    added new icon

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,10 +1,14 @@
 import TaskTable from '@/components/tasks/table';
 import TopBar from '@/components/tasks/top-bar';
+import { ClipboardList } from 'lucide-react';
 
 export default function TasksPage() {
   return (
     <main className="m-4">
-      <h1 className="text-3xl font-bold mb-4">Tasks</h1>
+      <h1 className="text-3xl font-bold mb-4 flex items-center gap-2">
+        <ClipboardList className="w-8 h-8 text-blue-600" />
+        Tasks
+      </h1>
       <TopBar></TopBar>
       <TaskTable />
     </main>
